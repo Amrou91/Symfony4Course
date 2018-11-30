9abfdb8a0d7546254dae2dda678f208ffb1d755d
cgarde <cristian.garde@gmail.com>
2018-11-30 15:36:14 +0000

    Token Validator using Event Subscriber

---
+++ 04.05.Token_Validator_Using_Event_Subscriber/src/Services/TokenValidatorService.php
@@ -0,0 +1,17 @@
+<?php
+// src/Services/TokenValidatorService.php
+namespace App\Services;
+
+class TokenValidatorService {
+    
+    public function validate(string $token) {
+        $authorizationType = 'Bearer';
+        $desiredToken = '123a';
+        
+        $tokenEvaluated = $authorizationType.' '.$desiredToken;
+        
+        $result = ($token === $tokenEvaluated) ? true : false;
+        
+        return $result;
+    }
+}
